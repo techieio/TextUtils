536643e30d21eb8c208bb9152b76d491a4666a66
Techie-io <programmertechie@gmail.com>
2022-07-27 15:54:30 +0530

    TextUtils

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
 
-function App() {
+export default function App() {
+  const [mode, SetMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      SetMode("dark");
+      document.body.style.backgroundColor = "#042743";
+    } else {
+      SetMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <TextForm
+        heading="TextUtils - Edit your text"
+        mode={mode}
+        toggleMode={toggleMode}
+        title="Your Text Summary"
+      />
+    </>
   );
 }
-
-export default App;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleRemoveSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
+  const handleClearText = () => {
+    setText("");
+  };
+
+  return (
+    <div
+      className="container"
+      style={{
+        color: props.mode === "dark" ? "white" : "#042743",
+        marginTop: "20px",
+      }}
+    >
+      <h1 className="text-center">{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="8 "
+          style={{
+            backgroundColor: props.mode === "dark" ? "#13466E" : "white",
+            color: props.mode === "dark" ? "white" : "#042743",
+            marginTop: "20px",
+            marginBottom: "17px",
+          }}
+        ></textarea>
+
+        <button
+          disabled={text.length == 0}
+          type="button"
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleUpClick}
+        >
+          Convert to UpperCase
+        </button>
+        <button
+          disabled={text.length == 0}
+          type="button"
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleLowClick}
+        >
+          Convert to LowerCase
+        </button>
+        <button
+          disabled={text.length == 0}
+          type="button"
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleRemoveSpace}
+        >
+          Remove Extra Space
+        </button>
+        <button
+          disabled={text.length == 0}
+          type="button"
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleClearText}
+        >
+          Clear Text
+        </button>
+      </div>
+      <div className="container my-3">
+      <h3 style={{marginTop: '50px', marginBottom: '13px'}}>{props.title}</h3>
+        <p>
+          {
+            text.split(/\s/).filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          Words and {text.length}characters
+        </p>
+        <p>
+          {0.008 *
+            text.split(/\s/).filter((element) => {
+              return element.length !== 0;
+            }).length}{" "}
+          Minutes to read
+        </p>
+      </div>
+    </div>
+  );
+}
